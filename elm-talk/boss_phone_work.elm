852f944aba7d7851d21f39bc855c71ffc58aef3f
Sergei Winitzki <sergei@versal.com>
2015-04-13 18:35:43 -0700

    fix boss_and_phone example in slides

--- elm-talk/boss_phone_work.elm
+++ elm-talk/boss_phone_work.elm
@@ -4,18 +4,20 @@ import Signal (..)
 import Graphics.Input as Input
 
 after_unless : (Bool, Bool) -> Bool -> Bool
-after_unless (b,r) w = (w || b) && not r
+after_unless (b,p) w = (w || b) && not p
 
-i_work : Signal Bool -> Signal Bool -> Signal Bool
-i_work boss phone = foldp after_unless False (map2 (\x y->(x,y)) boss phone)
+boss_and_phone : Signal (Bool, Bool)
+boss_and_phone = map2 (\x y->(x,y)) bossS phoneS
+
+i_work : Signal Bool
+i_work = foldp after_unless False boss_and_phone
 
 init_state = { boss = False, phone = False, work = False }
 
 main : Signal Element
 main = map draw merge_state
 
-merge_state = map3 (\x y z->{boss=x,phone=y,work=z})
-  bossS phoneS (i_work bossS phoneS)
+merge_state = map3 (\x y z->{boss=x,phone=y,work=z}) bossS phoneS i_work
 
 bossC = channel False
 phoneC = channel False
