c470d023bf2bfb68a9a49711ccabb762ef4df41f
Sergei Winitzki <sergei@versal.com>
2016-04-16 20:38:27 -0700

    add mercury files, initial commit for them

---
+++ mercury/factorial.m
@@ -0,0 +1,39 @@
+:- module factorial.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module list.
+:- import_module int.
+
+:- pred test_data(list(int)::out).
+
+test_data([1,2,3,4,5,6]).
+
+main(!IO) :-
+  % print factorial of 1, 2, 3, 4, 5, 6 using a predicate and using a function.
+  io.write_string("Table of factorials computed with the predicate:\n", !IO),
+  test_data(Test_data), map(factorial_pred, Test_data, R), io.write(R, !IO),
+  io.write_string("\n", !IO),
+  io.write_string("Table of factorials computed with the function:\n", !IO),
+  R2 = map(factorial_func, Test_data), io.write(R2, !IO),
+  io.write_string("\n", !IO).
+
+% Error: invalid determinism for `factorial_bad'(in) = out:
+%   the primary mode of a function cannot be `multi'.
+
+% :- func factorial_bad(int) = int.
+% factorial_bad(1) = 1.
+% factorial_bad(N) = N*factorial_bad(N-1).
+
+
+:- pred factorial_pred(int::in, int::out) is det.
+
+factorial_pred(N, F) :- ( if N =< 1 then F = 1 else factorial_pred(N-1, G), F = N * G ).
+
+:- func factorial_func(int) = int.
+
+factorial_func(N) = (if N =< 1 then 1 else N*factorial_func(N-1) ).
+
+% Note that factorial_pred and factorial_func result in the identical compiled code.
---
+++ mercury/martians.m
@@ -0,0 +1,23 @@
+:- module martians.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is cc_multi.
+
+:- implementation.
+:- import_module string.
+
+main(!IO) :-
+  io.write_string("Who is intelligent?\n", !IO),
+  (
+   if intelligent(X) 
+   then io.write_string(X, !IO) 
+   else io.write_string("Cannot determine!", !IO)
+  ),
+  nl(!IO).
+
+small("ngtrks"). green("ngtrks").
+martian("pgvdrk"). jumping("pgvdrk").
+green(X) :- jumping(X). 
+martian(X) :- small(X), jumping(X). 
+
+intelligent(X) :- green(X), martian(X).
